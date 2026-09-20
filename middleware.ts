1ff44d4cafac5a32d5a86b8668b5b858cedb7a19
tbw0391 <tbw0391@gmail.com>
2026-09-20 14:07:42 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -48,5 +48,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|manifest.json|icons|sw.js|workbox-.*).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|manifest.json|icons|branding|sw.js|workbox-.*).*)'],
 };
